8a7299da639200fbcb10641fd4e7062943eebc2f
capbussat <26734622+capbussat@users.noreply.github.com>
2026-07-14 16:41:18 +0200

    Check interface addresses
    Added error handling and user prompt for network check.

--- servidor-dhcp/servidor-dhcp.sh
+++ servidor-dhcp/servidor-dhcp.sh
@@ -1,5 +1,11 @@
 # servidor-dhcp.sh
+# si falla surt de l'script
+set -eu
 sudo apt update -y   
 sudo apt upgrade  
 sudo apt install kea-dhcp4-server -no-install-recommends  
 sudo apt autoremove  
+ip -c address show
+read -n 1 -p "Comprovem que hi ha enp1s0 amb default enp2s0 wireguard-vpn i dues xarxes persona 1 i personal 2 sense ip. Seguim (s/n)?"  tecla  
+
+

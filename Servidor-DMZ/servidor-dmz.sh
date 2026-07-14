b770b8be196112d8eeddbcb71f44e850a4bb593f
capbussat <26734622+capbussat@users.noreply.github.com>
2026-07-14 12:21:06 +0200

    Enhance servidor-dmz.sh with updated details and prompts
    Updated server description and connection details in the script. Improved prompts and comments for clarity.

--- Servidor-DMZ/servidor-dmz.sh
+++ Servidor-DMZ/servidor-dmz.sh
@@ -1,27 +1,30 @@
+#!/bin/bash
 cat << EOF
-Servidor DMZ
+Servidor DMZ (Debian Server 13)
+Aquest servidor estarà connectat al Servidor-router.
 Hauria de tenir una connexio enp1s0 Wireguard
-farem Una connexió enp2s0 manual. Aquest servidor estarà connectat al Servidor-router. 
-Tindrà una xarxa interna 192.168.10.0/24 La IP manual del servidor DMZ 192.168.10.2/24.
-Aquest Debian Server 13 està configurat amb Network Manager amb nmtui?"
+La connexió enp2s0 serà Manual. Tindrà una xarxa interna 192.168.10.0/24.
+La IP manual del servidor DMZ és 192.168.10.2/24.
+La xarxa està configurada amb Network Manager o nmtui?"
 EOF
 echo "systemctl is-active NetworkManager"
 systemctl is-active NetworkManager 
 echo "systemctl is-active systemd-networkd"
 systemctl is-active systemd-networkd 
-read -n 1  -p "Comprovem les connexions prem qualsevol tecla CTRL+C per sortir" tecla
+read -n 1  -p "Comprovem les connexions prem qualsevol tecla CTRL + C per sortir" tecla
+ECHO "la connexió \"internet\" és enp2s0 amb Address=192.168.10.2 Gateway=192.168.10.1 i DNS=1.1.1.1?"
 nmcli connection show
-# la connexió "internet" és enp2s0 amb Address=192.168.10.2 Gateway=192.168.10.1 i DNS=1.1.1.1.
-read -n 1  -p "Prem qualsevol tecla per continuar CTRL+C per sortir" tecla
+read -n 1  -p "Prem qualsevol tecla per continuar CTRL + C per sortir" tecla
 sudo nmcli connection modify "internet" connection.interface-name enp2s0;
 sudo nmcli connection modify "internet" ipv4.addresses 192.168.10.2/24 ipv4.gateway 192.168.10.1 ipv4.dns 1.1.1.1;
 sudo nmcli connection up "internet";
 
 echo "Actualitza i instal·la Nginx"
-read -n 1  -p "Prem qualsevol tecla per continuar CTRL+C per sortir" tecla
+read -n 1  -p "Prem qualsevol tecla per continuar CTRL + C per sortir" tecla
 sudo apt update -y && sudo apt upgrade
 sudo apt install nginx
 sudo apt autoremove
+
 echo "Comprova  nginx"
 sudo systemctl status nginx
 

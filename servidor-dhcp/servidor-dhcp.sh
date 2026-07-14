ce698d1c277bceac9486491b9c64c40a8974e391
capbussat <26734622+capbussat@users.noreply.github.com>
2026-07-14 16:44:15 +0200

    Enhance servidor-dhcp.sh with server description
    Added a description block for the DHCP server setup.

--- servidor-dhcp/servidor-dhcp.sh
+++ servidor-dhcp/servidor-dhcp.sh
@@ -1,6 +1,15 @@
 # servidor-dhcp.sh
 # si falla surt de l'script
 set -eu
+cat << EOF
+Servidor DHCP (Ubuntu 24)
+Aquest servidor serà el servidor principal.  
+Hauria de tenir una connexio   
+enp1s0 default  
+enp2s0 Wireguard  
+enp3s0 Personal1
+enp4s0 Personal2 
+EOF
 sudo apt update -y   
 sudo apt upgrade  
 sudo apt install kea-dhcp4-server -no-install-recommends  

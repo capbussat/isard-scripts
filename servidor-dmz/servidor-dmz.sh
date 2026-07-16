75a453134ba589e2e4466570b87163e3fbbbeded
capbussat <26734622+capbussat@users.noreply.github.com>
2026-07-16 11:38:08 +0200

    Update DMZ server configuration for new network settings

--- servidor-dmz/servidor-dmz.sh
+++ servidor-dmz/servidor-dmz.sh
@@ -3,8 +3,8 @@ cat << EOF
 Servidor DMZ (Debian Server 13)
 Aquest servidor estarà connectat al Servidor-router.
 Hauria de tenir una connexio enp1s0 Wireguard
-La connexió enp2s0 serà Manual. Tindrà una xarxa interna 192.168.10.0/24.
-La IP manual del servidor DMZ és 192.168.10.2/24.
+La connexió enp2s0 serà Manual. Tindrà una xarxa interna 192.168.12.0/24.
+La IP manual del servidor DMZ és 192.168.12.2/24.
 La xarxa està configurada amb Network Manager o nmtui?"
 EOF
 echo "systemctl is-active NetworkManager"
@@ -12,11 +12,11 @@ systemctl is-active NetworkManager
 echo "systemctl is-active systemd-networkd"
 systemctl is-active systemd-networkd 
 read -n 1  -p "Comprovem les connexions prem qualsevol tecla CTRL + C per sortir" tecla
-ECHO "la connexió \"internet\" és enp2s0 amb Address=192.168.10.2 Gateway=192.168.10.1 i DNS=1.1.1.1?"
+ECHO "la connexió \"internet\" és enp2s0 amb Address=192.168.12.2 Gateway=192.168.12.1 i DNS=1.1.1.1?"
 nmcli connection show
 read -n 1  -p "Prem qualsevol tecla per continuar CTRL + C per sortir" tecla
 sudo nmcli connection modify "internet" connection.interface-name enp2s0;
-sudo nmcli connection modify "internet" ipv4.addresses 192.168.10.2/24 ipv4.gateway 192.168.10.1 ipv4.dns 1.1.1.1;
+sudo nmcli connection modify "internet" ipv4.addresses 192.168.12.2/24 ipv4.gateway 192.168.12.1 ipv4.dns 1.1.1.1;
 sudo nmcli connection up "internet";
 
 echo "Actualitza i instal·la Nginx"
@@ -28,8 +28,8 @@ sudo apt autoremove
 echo "Comprova  nginx"
 sudo systemctl status nginx
 
-echo "Comprova ping 192.168.10.1 gateway"
-ping -c 4 192.168.10.1
+echo "Comprova ping 192.168.12.1 gateway"
+ping -c 4 192.168.12.1
 echo "Comprova  ping 10.10.10.1 router, per l'altra xarxa"
 ping -c 4 10.10.10.1
 echo "Comprova accés a internet"

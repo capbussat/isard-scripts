922a5822efc7c832fcec186842f395dfd8568d75
capbussat <26734622+capbussat@users.noreply.github.com>
2026-07-21 18:10:32 +0200

    Add sudo to Zabbix installation and MySQL configuration

--- servidor-zabbix/servidor-zabbix.sh
+++ servidor-zabbix/servidor-zabbix.sh
@@ -26,13 +26,14 @@ demanar_confirmacio (){
 install_zabbix(){
 echo "Install Zabbix"
 wget https://repo.zabbix.com/zabbix/7.4/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.4+debian12_all.deb  
-dpkg -i zabbix-release_latest_7.4+debian12_all.deb  
-apt update -y    
-apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent  
+sudo dpkg -i zabbix-release_latest_7.4+debian12_all.deb  
+sudo apt update -y    
+sudo apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent  
 }
 
 install_mysql(){
 echo "Instal·la MySQL database..."    
+sudo apt install mariadb-server mariadb-client
 }
 
 configure_mysql(){  
@@ -40,7 +41,7 @@ echo "Configure MySQL database..."
 read -sp "Crea una contrasenya root MySQL: " ROOT_PASS
 echo
 DB_PASS='password'  
-mysql -uroot -p"$ROOT_PASS" << EOF  
+sudo mysql -uroot -p"$ROOT_PASS" << EOF  
 CREATE DATABASE zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;  
 CREATE USER zabbix@localhost IDENTIFIED BY '${DB_PASS}';  
 GRANT ALL PRIVILEGES ON zabbix.* TO zabbix@localhost;  

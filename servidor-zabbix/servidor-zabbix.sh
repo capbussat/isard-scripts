3c32d515005f25f778a9841fa762b64901eb1275
capbussat <26734622+capbussat@users.noreply.github.com>
2026-07-21 17:51:02 +0200

    Install Zabbix server and related packages

--- servidor-zabbix/servidor-zabbix.sh
+++ servidor-zabbix/servidor-zabbix.sh
@@ -4,3 +4,4 @@
 wget https://repo.zabbix.com/zabbix/7.4/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.4+debian12_all.deb  
 dpkg -i zabbix-release_latest_7.4+debian12_all.deb  
 apt update -y    
+apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent  

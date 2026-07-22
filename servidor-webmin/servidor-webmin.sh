b8f06d5a40e10f648a84a490586949a766876ede
capbussat <26734622+capbussat@users.noreply.github.com>
2026-07-22 14:20:35 +0200

    Enhance servidor-webmin.sh with Webmin installation
    Added installation steps for Webmin and user setup.

---
+++ servidor-webmin/servidor-webmin.sh
@@ -0,0 +1,10 @@
+#!/bin.bash
+# servidor-webmin.sh
+
+curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh  
+sudo sh webmin-setup-repo.sh  
+sudo apt-get install webmin --install-recommends
+echo " Canvia la contrasenya per root:"
+sudo webmin passwd root 
+echo "Podeu accedir a la seva interfície introduint firefox localhost:10000 usuari root"  
+echo "Crea els grups i isusaris necessaris"    

8b7b1089d0e3f7b2cd4dbceccea1263b96a53eb1
capbussat <26734622+capbussat@users.noreply.github.com>
2026-07-23 13:58:25 +0200

    Refactor Borg backup script with confirmation prompts

--- client-borg/client-borg.sh
+++ client-borg/client-borg.sh
@@ -4,6 +4,26 @@ set -peu
 
 export BORG_REPO=ssh://isard@192.168.10.1:22/~/.backups/
 
+seguim(){
+    read -n 1 -p "$1" tecla
+    if [[ $tecla == [sS] ]]; then
+        echo            
+        echo "Sortint de l'script."
+        exit 1
+    else 
+        echo "..."
+    fi  
+}
+
+demana_confirmacio (){
+    echo 
+    read -n 1 -p "$1" tecla    
+    if [[ $tecla == [sS] ]]; then
+        echo "..."
+        "$2"
+    fi
+}
+
 cat << EOF
 Client Borg backup que copiarà per SSH al servidor dhcp.  
 Instal·la borg backup.
@@ -14,6 +34,9 @@ ssh_keys(){
 echo "Crea i copia les claus SSH"
 ssh-keygen -t ed25519
 ssh-copy-id isard@192.168.10.1
+}
+
+ssh_check(){
 echo "Comprova l'accés"
 ssh isard@192.168.10.1
 echo "Crea el directori remot"
@@ -22,18 +45,20 @@ ssh isard@192.168.10.1 "mkdir -p -m 700 ~/.backups"
 ssh isard@192.168.10.1 "ls -la ~/.backups"
 }
 
-install(){
+borg_install(){
 sudo apt update -y
 sudo apt install borgbackup
 sudo apt autoremove
 }
 
-check(){
+borg_check(){
 touch checkborg.txt
 borg create $BORG_REPO::check checkborg.txt
 borg list check
 }
 
-ssh_keys
-install
+demana_confirmacio "Vols crear claus SSH? (s/n)" ssh_keys
+demana_confirmacio "Vols provar les claus SSH? (s/n)" ssh_check
+demana_confirmacio "Vols instal·lar Borg backup? (s/n)" borg_install
+demana_confirmacio "Vols provar Borg backup (s/n)?"  borg_check
 check

381c3a4326166b2f1883a19526f97b91450cf0a1
capbussat <26734622+capbussat@users.noreply.github.com>
2026-07-23 11:24:14 +0200

    Enhance servidor-webmin.sh with repo setup and cleanup
    Updated script to include repository setup and cleanup commands.

--- servidor-webmin/servidor-webmin.sh
+++ servidor-webmin/servidor-webmin.sh
@@ -1,10 +1,12 @@
 #!/bin.bash
-# servidor-webmin.sh
-
+# servidor-webmin.sh  
+sudo apt update -y   
+sudo apt upgrade  
 curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh  
 sudo sh webmin-setup-repo.sh  
-sudo apt-get install webmin --install-recommends
-echo " Canvia la contrasenya per root:"
+sudo apt-get install webmin --install-recommends  
+sudo apt autoremove  
+echo " Canvia la contrasenya per root:"  
 sudo webmin passwd root 
 echo "Podeu accedir a la seva interfície introduint firefox localhost:10000 usuari root"  
 echo "Crea els grups i i usaris necessaris"    

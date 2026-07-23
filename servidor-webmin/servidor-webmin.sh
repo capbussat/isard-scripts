e096198af8677f375d3cd486b18f37972aeac2ba
capbussat <26734622+capbussat@users.noreply.github.com>
2026-07-23 11:27:04 +0200

    Enhance user feedback in servidor-webmin.sh
    Added echo statements for better user feedback during installation.

--- servidor-webmin/servidor-webmin.sh
+++ servidor-webmin/servidor-webmin.sh
@@ -2,11 +2,14 @@
 # servidor-webmin.sh  
 sudo apt update -y   
 sudo apt upgrade  
+echo "Descarrega webmin ..."  
 curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh  
+echo "Instal·la webmin ..."  
 sudo sh webmin-setup-repo.sh  
 sudo apt-get install webmin --install-recommends  
+echo "Neteja ..."  
 sudo apt autoremove  
-echo " Canvia la contrasenya per root:"  
+echo "Canvia la contrasenya per root:"  
 sudo webmin passwd root 
 echo "Podeu accedir a la seva interfície introduint firefox localhost:10000 usuari:root amb la contrasenya que has creat."  
 echo "Crea els grups i i usuaris necessaris."    
